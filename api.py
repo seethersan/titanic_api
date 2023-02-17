6f1d124e7838518299f32605b3cb39f864705dfa
Juan Carlos Espinoza Zurita <carlos_jcez@hotmail.com>
2023-02-17 10:40:37 -0500

    add missing column handling

--- api.py
+++ api.py
@@ -1,3 +1,4 @@
+import traceback
 from flask import Flask, jsonify, request
 import pandas as pd
 import joblib
@@ -6,14 +7,22 @@ import joblib
 app = Flask(__name__)
 
 lr = joblib.load('model.pkl')
+model_columns = joblib.load('model_columns.pkl')
 
 
 @app.route('/predict', methods=['GET'])
 def predict():
-    json = request.json
-    query_df = pd.DataFrame(json)
-    query = pd.get_dummies(query_df)
+    if lr:
+        try:
+            json = request.json
+            query = pd.get_dummies(pd.DataFrame(json))
+            query = query.reindex(columns=model_columns, fill_value=0)
 
-    prediction = lr.predict(query)
-    return jsonify({'prediction': list(prediction)})
+            prediction = lr.predict(query)
+            return jsonify({'prediction': list(prediction)})
+        except:
+            return jsonify({'trace': traceback.format_exc()})
+    else:
+        print('Train the model first')
+        return ('No model here to use')
 
